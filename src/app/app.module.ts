29bd987b15d4301f04dd07b687bf17c2dc90af71
Salma73480 <201821820@tese.edu.mx>
2021-04-19 21:13:43 -0500

    Borrar Heroe y listado de Heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
+import { HeroeComponent } from './heroes/heroe/heroe.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListadoComponent } from './heroes/listado/listado.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+HeroeComponent,
+ListadoComponent,
+],
+
+imports: [
+BrowserModule,
+AppRoutingModule
+],
+
+providers: [],
+bootstrap: [AppComponent]
+
 })
+
 export class AppModule { }
