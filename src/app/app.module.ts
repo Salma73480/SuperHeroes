59091a554a69068f80e3e332ad2be766abe8c61c
Salma73480 <201821820@tese.edu.mx>
2021-04-21 14:31:29 -0500

    Agregamos el modulo Contador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListadoComponent } from './heroes/listado/listado.component';
-
+import { ContadorComponent} from './contador/contador.component';
 
 @NgModule({
 declarations: [
 AppComponent,
 HeroeComponent,
 ListadoComponent,
+ContadorComponent,
 ],
 
 imports: [
